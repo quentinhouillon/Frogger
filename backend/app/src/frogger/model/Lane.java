db61de6ef81da99d0b1b07639f69d68da313cd9c
laetitia <akilallaeticia@gmail.com>
2026-02-19 11:10:07 +0100

    Feat: Ajout du model Lane

---
+++ backend/app/src/frogger/model/Lane.java
@@ -0,0 +1,11 @@
+public class Lane {
+
+    private int position;
+    private boolean Obstacle;
+
+    public Lane(int position , boolean Obstacle) 
+    {
+        this.position=position;
+        this.Obstacle=Obstacle;
+    }
+}
