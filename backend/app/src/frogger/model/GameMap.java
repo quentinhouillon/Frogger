612650f694ab90c1ef733ed03dc9a4ab836e7444
laetitia <akilallaeticia@gmail.com>
2026-02-19 09:46:52 +0100

    feat : ajouter GameMap

---
+++ backend/app/src/frogger/model/GameMap.java
@@ -0,0 +1,29 @@
+public class GameMap {
+
+    private int width;
+    private int height;
+
+    public void display() {
+    }
+
+    public boolean isInside(int x, int y) {
+        return false;
+    }
+
+    public void start() {
+    }
+
+    public void stop() {
+    }
+
+    public void reload() {
+    }
+
+    public boolean checkCollision() {
+        return false;
+    }
+
+    public boolean checkWin() {
+        return false;
+    }
+}
